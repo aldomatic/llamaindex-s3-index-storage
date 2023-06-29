eecd8eca39f905b7b4adbb7e41a4e992b826ddd4
Aldo Lugo <aldo@aldomatic.com>
2023-06-28 22:11:00 -0500

    updated logic

--- app.py
+++ app.py
@@ -3,28 +3,42 @@ import s3fs
 import os
 import openai
 from dotenv import load_dotenv
-from llama_index import load_index_from_storage, StorageContext
+from llama_index import load_index_from_storage, StorageContext, Document
 
+def persist_storage(index, s3, s3_bucket):
+    # persist storage
+    index.storage_context.persist(s3_bucket, fs=s3)
+
+def index_storage(s3, s3_bucket):
+    # load index from s3
+    storage_context = StorageContext.from_defaults(persist_dir=s3_bucket, fs=s3)
+    # return index
+    return load_index_from_storage(storage_context, 'vector_index')
+
+def refresh_store(index, documents):
+    refresh_doc = index.refresh_ref_docs(documents)
+
+def process_to_documents(docs):
+    for uploaded_file in docs:
+        document_text = uploaded_file.read()
+        st.write("filename:", uploaded_file.name)
+        return [Document(text=document_text.decode('utf-8'))]
 
 def handle_userinput(user_question, index):
     query_engine = index.as_query_engine()
     result = query_engine.query(user_question)
     st.write(result.response)
 
-
 def main():
     load_dotenv()
 
-    openai.api_key= os.getenv('OPENAI_API_KEY', default='MISSING KEY')
-
     AWS_KEY= os.getenv('AWS_KEY', default='MISSING KEY')
     AWS_SECRET= os.getenv('AWS_SECRET', default='MISSING KEY')
-    BUCKET= os.getenv('BUCKET', default='MISSING KEY')
-    KEY= os.getenv('BUCKET_STORAGE_KEY', default='MISSING KEY')
+    s3_bucket= os.getenv('BUCKET', default='MISSING KEY')
 
-    # set page title
-    st.set_page_config(page_title="Llamaindex + S3 index storage", page_icon=None, layout="centered", initial_sidebar_state="auto", menu_items=None)
+    openai.api_key= os.getenv('OPENAI_API_KEY', default='MISSING KEY')
 
+    # setup S3 file system
     s3 = s3fs.S3FileSystem(
         key=AWS_KEY,
         secret=AWS_SECRET,
@@ -32,18 +46,36 @@ def main():
         s3_additional_kwargs={'ACL': 'public-read-write'}
     )
 
-    # load index from s3
-    persist_dir = f'{BUCKET}/{KEY}'
-    sc = StorageContext.from_defaults(persist_dir=persist_dir, fs=s3)
-
-    index = load_index_from_storage(sc, 'vector_index')
+    # set page title
+    st.set_page_config(page_title="Llamaindex + S3 index storage", page_icon=None, layout="centered", initial_sidebar_state="auto", menu_items=None)
 
     st.header("What would you like to know?")
 
+    # capture user query
     user_question = st.text_input("Ask a question about your documents:")
+
+    # get storage index from s3
+    index = index_storage(s3, s3_bucket)
     
     if user_question:
         handle_userinput(user_question, index)
+
+    with st.sidebar:
+        st.subheader("Your documents")
+        uploaded_docs = st.file_uploader(
+            "Upload your files here and click on 'Process'", accept_multiple_files=True)
+        if st.button("Process"):
+            with st.spinner("Processing"):
+
+                # process the uploaded files and convert to documents
+                docs = process_to_documents(uploaded_docs)
+            
+                # refresh the store with the new documents
+                refresh_store(index, docs)
+
+                # persist updated store
+                persist_storage(index, s3, s3_bucket)
+
 
 if __name__ == '__main__':
     main()
